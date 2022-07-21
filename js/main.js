0aaff28a9e3d8edf027903dd3ec6346e4800f353
Toruy385 <76161357+Toruy385@users.noreply.github.com>
2022-07-21 13:48:16 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(function () {
+  // スムーススクロール
+  // ページ内リンクをクリックした時に動作
+  $('a[href^="#"').click(function() {
+    // リンクを取得
+    let href = $(this).attr("href");
+    // ジャンプ先のid名をセット
+    let target = $(href == "#" || href == "" ? 'html' : href);
+    // トップからジャンプまでの要素の距離を取得
+    let position = target.offset().top;
+    // animateでスムーススクロールを行う
+    // 600はスクロール速度で単位はミリ秒
+    $("html, body").animate({scrollTop:position}, 600, "swing");
+    return false;
+  });
+})
